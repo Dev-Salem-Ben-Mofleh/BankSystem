5dda612c922325995ef4ca03eb2ddaf3c12d8c90
Salem Ben Mofleh <salembenmofleh@gmail.com>
2024-07-06 13:46:15 +0300

    ADD Project File

---
+++ BankSystem/BankSystem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+using namespace std;
+
+int main()
+
+{
+    while(true)
+    {
+       
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+        
+    }
+
+    
+    return 0;
+}
